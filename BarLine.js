10f9c501beb149bae50b7fed2a5be8e77fad12ce
thayer <ackermanth@appstate.edu>
2024-05-08 13:22:52 -0400

    barline updated with Feng's feedback and new color scheme

--- BarLine.js
+++ BarLine.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+var margin = {top: 15, right: 50, bottom: 50, left: 50},
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -15,7 +15,11 @@ var svg = d3.select("#barline")
 //Read the data
 d3.csv("Video_Games.csv").then(data => {
 
-    var gameSales = {};
+    var globalGameSales = {};
+    var naGameSales = {};
+    var euGameSales = {};
+    var jpGameSales = {};
+    var otherGameSales = {};
     var criticScores = {};
     var userScores = {};
 
@@ -35,8 +39,20 @@ d3.csv("Video_Games.csv").then(data => {
             }
         }
         
-        if (year in gameSales) gameSales[year] += parseFloat(data[j].Global_Sales);
-        else gameSales[year] = parseFloat(data[j].Global_Sales);
+        if (year in globalGameSales) globalGameSales[year] += parseFloat(data[j].Global_Sales);
+        else globalGameSales[year] = parseFloat(data[j].Global_Sales);
+
+        if (year in naGameSales) naGameSales[year] += parseFloat(data[j].NA_Sales);
+        else naGameSales[year] = parseFloat(data[j].NA_Sales);
+
+        if (year in euGameSales) euGameSales[year] += parseFloat(data[j].EU_Sales);
+        else euGameSales[year] = parseFloat(data[j].EU_Sales);
+
+        if (year in jpGameSales) jpGameSales[year] += parseFloat(data[j].JP_Sales);
+        else jpGameSales[year] = parseFloat(data[j].JP_Sales);
+
+        if (year in otherGameSales) otherGameSales[year] += parseFloat(data[j].Other_Sales);
+        else otherGameSales[year] = parseFloat(data[j].Other_Sales);
       }
     }
 
@@ -47,8 +63,8 @@ d3.csv("Video_Games.csv").then(data => {
     criticScore: criticScores[year]
   }));
 
-    const years = Object.keys(gameSales).map(str => parseInt(str));
-    const sales = Object.values(gameSales);
+    const years = Object.keys(globalGameSales).map(str => parseInt(str));
+    const sales = Object.values(globalGameSales);
 
     const gameSalesObject = years.reduce((obj, key, index) => {
       obj[key] = sales[index];
@@ -62,22 +78,46 @@ d3.csv("Video_Games.csv").then(data => {
       .domain([d3.min(years), d3.max(years)])
       .range([ 0, width ]);
     svg.append("g")
+      .attr("stroke", "lightgray")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
+    svg.append("text")
+      .attr("fill", "lightgray")
+      .attr("class", "y label")
+      .attr("dy", ".75em")
+      .attr("transform", "translate( " +  width/2.5 + "," + (height+25) + ")")
+      .text("Release (year)");
 
     // Add Y axis
     var y = d3.scaleLinear()
       .domain([0, d3.max(sales)])
       .range([ height, 0 ]);
     svg.append("g")
+      .attr("stroke", "lightgray")
       .call(d3.axisLeft(y));
+    svg.append("text")
+      .attr("fill", "lightgray")
+      .attr("class", "y label")
+      .attr("dy", ".75em")
+      .attr("transform", "translate( -50," + height/1.5 + ")" + "rotate(-90)")
+      .text("Games Sales (millions)");
 
     
     console.log(scores);
     // Add Y axis
     var y2 = d3.scaleLinear()
-      .domain([0, d3.max(scores, d => d.userScore + d.criticScore)])
-      .range([ height, 300]);
+      .domain([0, 200])
+      .range([height, 0]);
+    svg.append("g")
+      .attr("transform", "translate(" + width + ",0)")
+      .attr("stroke", "lightgray")
+      .call(d3.axisRight(y2));
+    svg.append("text")
+      .attr("fill", "lightgray")
+      .attr("class", "y label")
+      .attr("dy", ".75em")
+      .attr("transform", "translate( " + (width-25) + "," + height/1.5 + ")" + "rotate(-90)")
+      .text("Scores (0-100)");
 
     // Draw stacked bars
     const barWidth = width / scores.length; // Calculate the width of each bar
@@ -94,7 +134,7 @@ d3.csv("Video_Games.csv").then(data => {
     .attr("y", d => y2(d.userScore))
     .attr("width", barWidth / 2) // Set the width of user bars
     .attr("height", d => height - y2(d.userScore))
-    .attr("fill", "red");
+    .attr("fill", "#9084b5");
 
     barGroups.append("rect")
     .attr("class", "critic-bar")
@@ -102,15 +142,15 @@ d3.csv("Video_Games.csv").then(data => {
     .attr("y", d => y2(d.userScore + d.criticScore))
     .attr("width", barWidth / 2) // Set the width of critic bars
     .attr("height", d => height - y2(d.criticScore))
-    .attr("fill", "blue");
+    .attr("fill", "#84a7b5");
 
     console.log(gameSalesArray);
     // Add the line
     svg.append("path")
       .datum(gameSalesArray) // Use the array of key-value pairs
       .attr("fill", "none")
-      .attr("stroke", "green")
-      .attr("stroke-width", 4)
+      .attr("stroke", "#ffdede")
+      .attr("stroke-width", 8)
       .attr("d", d3.line()
         .x(function(d) { return x(d[0]); }) // Accessing the year from the key
         .y(function(d) { return y(d[1]); }) // Accessing the sales from the value
